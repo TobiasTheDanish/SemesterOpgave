323c45462b615fdca2f5f2ddda43dc86b1106115
TobiasTheDanish <t.hc199811@gmail.com>
2023-05-08 14:31:28 +0200

    Added getOrderById function

--- src/main/java/dat/backend/model/persistence/OrderFacade.java
+++ src/main/java/dat/backend/model/persistence/OrderFacade.java
@@ -9,4 +9,8 @@ public class OrderFacade {
     public static List<Order> getAllOrders(ConnectionPool connectionPool) throws DatabaseException {
         return OrderMapper.getAllOrders(connectionPool);
     }
+
+    public static Order getOrderById(int id, ConnectionPool connectionPool) throws DatabaseException {
+        return OrderMapper.getOrderById(id, connectionPool);
+    }
 }
--- src/main/java/dat/backend/model/persistence/OrderMapper.java
+++ src/main/java/dat/backend/model/persistence/OrderMapper.java
@@ -47,6 +47,43 @@ class OrderMapper {
         return result;
     }
 
+    protected static Order getOrderById(int id, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "SELECT * FROM semesteropgave.order WHERE order_id=?";
+
+        try (Connection connection = connectionPool.getConnection()) {
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ps.setInt(1, id);
+
+                ResultSet rs = ps.executeQuery();
+
+                if (rs.next()) {
+
+                    return getOrderFromResultSet(rs, id, connectionPool);
+                }
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+
+        return null;
+    }
+
+    private static Order getOrderFromResultSet(ResultSet rs, int id, ConnectionPool connectionPool) throws SQLException, DatabaseException {
+        int userId = rs.getInt("user_id");
+        int status = rs.getInt("status");
+        int width = rs.getInt("width");
+        int height = rs.getInt("height");
+        int length = rs.getInt("length");
+
+        User user = UserMapper.getUserById(userId, connectionPool);
+
+        Order order = new Order(user, status, width, height,length);
+        order.setId(id);
+        order.setMaterials(getOrderMaterials(id, connectionPool));
+
+        return order;
+    }
+
     protected static List<Pair<Material, Integer>> getOrderMaterials(int id, ConnectionPool connectionPool) throws DatabaseException {
         List<Pair<Material, Integer>> materials = new ArrayList<>();
 
