66091e1fef7bae4ea37a35848a3b8e9687a30a8c
TobiasTheDanish <t.hc199811@gmail.com>
2023-05-15 22:39:48 +0200

    Extracted material calculations to functions to easier allow for unit tests

--- src/main/java/dat/backend/model/services/MaterialListAlgorithm.java
+++ src/main/java/dat/backend/model/services/MaterialListAlgorithm.java
@@ -19,28 +19,39 @@ public class MaterialListAlgorithm {
         int height = order.getHeight();
         int width = order.getWidth();
 
-        //Calculating length of posts
-            //Maximum length between two posts is 310 cm.
-            int amountOfPosts = ((int) Math.ceil((double) length / 310) + 1) * 2;
-            Material post = MaterialFacade.getMaterialByNameAndLength("Stolpe", height, connectionPool);
-            Pair<Material, Integer> posts = new Pair<>(post, amountOfPosts);
+        Pair<Material, Integer> posts = getPosts(connectionPool, length, height);
+        Pair<Material, Integer> rafters = getRafters(connectionPool, length, width);
+        Pair<Material, Integer> purlins = getPurlins(connectionPool, length);
 
-        //Calculating length of rafters
-            //Maximum length between two rafters is 55 cm.
-            int amountOfRafters = (int) Math.floor((double) length / 55);
-            Material rafter = MaterialFacade.getMaterialByNameAndLength("Spær", width, connectionPool);
-            Pair<Material, Integer> rafters = new Pair<>(rafter, amountOfRafters);
+        List<Pair<Material, Integer>> listOfPairs = new ArrayList<>();
+        listOfPairs.add(posts);
+        listOfPairs.add(rafters);
+        listOfPairs.add(purlins);
+        MaterialFacade.createOrderLink(order, listOfPairs, connectionPool);
+    }
 
-        //Calculating length of purlins
-            int amountOfPurlins = 2;
-            Material purlin = MaterialFacade.getMaterialByNameAndLength("Rem", length, connectionPool);
-            Pair<Material, Integer> purlins = new Pair<>(purlin, amountOfPurlins);
+    //Calculating length of purlins
+    public static Pair<Material, Integer> getPurlins(ConnectionPool connectionPool, int length) throws DatabaseException {
+        //Since we only ever need 1 purlin on each side, the amount is always 2.
+        //We keep the variable however, to make it easier to refactor later, if needed.
+        int amountOfPurlins = 2;
+        Material purlin = MaterialFacade.getMaterialByNameAndLength("Rem", length, connectionPool);
+        return new Pair<>(purlin, amountOfPurlins);
+    }
 
+    //Calculating length and amount of rafters need
+    public static Pair<Material, Integer> getRafters(ConnectionPool connectionPool, int length, int width) throws DatabaseException {
+        //Maximum length between two rafters is 55 cm.
+        int amountOfRafters = (int) Math.floor(((double) length) / 55);
+        Material rafter = MaterialFacade.getMaterialByNameAndLength("Spær", width, connectionPool);
+        return new Pair<>(rafter, amountOfRafters);
+    }
 
-            List<Pair<Material, Integer>> listOfPairs = new ArrayList<>();
-            listOfPairs.add(posts);
-            listOfPairs.add(rafters);
-            listOfPairs.add(purlins);
-            MaterialFacade.createOrderLink(order, listOfPairs, connectionPool);
+    //Calculating length and amount of posts need
+    public static Pair<Material, Integer> getPosts(ConnectionPool connectionPool, int length, int height) throws DatabaseException {
+        //Maximum length between two posts is 310 cm.
+        int amountOfPosts = ((int) Math.ceil(((double) length) / 310) + 1) * 2;
+        Material post = MaterialFacade.getMaterialByNameAndLength("Stolpe", height, connectionPool);
+        return new Pair<>(post, amountOfPosts);
     }
 }
