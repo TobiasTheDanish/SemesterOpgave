cc55031073b36e031f5f9c76fd2ddc1be581c6ae
snadering <cph-sr319@cphbusiness.dk>
2023-05-15 09:07:11 +0200

    algorithm class created

--- src/main/java/dat/backend/model/persistence/MaterialMapper.java
+++ src/main/java/dat/backend/model/persistence/MaterialMapper.java
@@ -34,4 +34,6 @@ class MaterialMapper {
         }
         return null;
     }
+
+
 }
---
+++ src/main/java/dat/backend/model/services/MaterialListAlgorithm.java
@@ -0,0 +1,20 @@
+package dat.backend.model.services;
+
+import dat.backend.model.entities.Order;
+
+public class MaterialListAlgorithm {
+
+    public void calcMaterialList(Order order) {
+        int length = order.getLength();
+        int height = order.getHeight();
+        int width = order.getWidth();
+
+        //TODO: Calc amount of stolper by dividing length with 310
+
+        //TODO: Calculate length of spær
+
+        //TODO: calculate length of remme
+
+
+    }
+}
