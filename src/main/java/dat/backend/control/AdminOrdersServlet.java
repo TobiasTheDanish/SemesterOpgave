30db87aabca3cb3f30917783b3cf024d7a4e6277
TobiasTheDanish <t.hc199811@gmail.com>
2023-05-18 14:14:12 +0200

    Further optimizations to AdminOrdersServlet. This time to how Materials are fetched

--- src/main/java/dat/backend/control/AdminOrdersServlet.java
+++ src/main/java/dat/backend/control/AdminOrdersServlet.java
@@ -5,7 +5,9 @@ import dat.backend.model.entities.Material;
 import dat.backend.model.entities.Order;
 import dat.backend.model.exceptions.DatabaseException;
 import dat.backend.model.persistence.ConnectionPool;
+import dat.backend.model.persistence.MaterialFacade;
 import dat.backend.model.persistence.OrderFacade;
+import org.javatuples.Pair;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -13,56 +15,43 @@ import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @WebServlet(name = "adminordersservlet", value = "/adminordersservlet")
 public class AdminOrdersServlet extends HttpServlet {
     private ConnectionPool connectionPool;
-    private long lastGetOrdersCall;
-    private List<Order> orders;
 
     @Override
     public void init()
     {
         this.connectionPool = ApplicationStart.getConnectionPool();
-        this.lastGetOrdersCall = 0;
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
-        orders = (List<Order>) session.getAttribute("orders");
+        List<Order> orders = (List<Order>) session.getAttribute("orders");
 
-        if (orders == null || orders.size() == 0 || shouldGetOrders()) {
-            try {
-                orders = OrderFacade.getAllOrdersWithoutMaterials(connectionPool);
-                lastGetOrdersCall = System.currentTimeMillis();
+        try {
+            orders = OrderFacade.getAllOrdersWithoutMaterials(connectionPool);
 
-                session.setAttribute("orders", orders);
-                request.getRequestDispatcher("WEB-INF/adminOrders.jsp").forward(request, response);
-            } catch (DatabaseException e) {
-                e.printStackTrace();
-                request.setAttribute("errormessage", "Der skete en fejl under hentningen af ordrer fra databasen:\n" + e.getMessage());
-                request.getRequestDispatcher("error.jsp").forward(request, response);
-            }
-        } else {
+            session.setAttribute("orders", orders);
+
             request.getRequestDispatcher("WEB-INF/adminOrders.jsp").forward(request, response);
+        } catch (DatabaseException e) {
+            e.printStackTrace();
+            request.setAttribute("errormessage", "Der skete en fejl under hentningen af ordrer fra databasen:\n" + e.getMessage());
+            request.getRequestDispatcher("error.jsp").forward(request, response);
         }
 
-        for (Order order : orders) {
-            try {
-                order.setMaterials(OrderFacade.getOrderMaterials(order.getId(), connectionPool));
-            } catch (DatabaseException e) {
-                e.printStackTrace();
+        try {
+            Map<Integer, List<Pair<Material, Integer>>> orderLinkMap = MaterialFacade.getOrderLinkMap(connectionPool);
+
+            for (Order order : orders) {
+                order.setMaterials(orderLinkMap.get(order.getId()));
             }
+        } catch (DatabaseException e) {
+            e.printStackTrace();
         }
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-
-    private boolean shouldGetOrders() {
-        return lastGetOrdersCall + 5000 <= System.currentTimeMillis();
     }
 }
--- src/main/java/dat/backend/model/persistence/MaterialMapper.java
+++ src/main/java/dat/backend/model/persistence/MaterialMapper.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
@@ -88,19 +89,44 @@ class MaterialMapper {
 
 
 
-    protected static Map<Integer, Pair<Integer, Material>> getOrderLinkMap(ConnectionPool connectionPool) throws DatabaseException {
-        try {
-            getMaterialMap(connectionPool.getConnection());
+    protected static Map<Integer, List<Pair<Material, Integer>>> getOrderLinkMap(ConnectionPool connectionPool) throws DatabaseException {
+        Map<Integer, List<Pair<Material, Integer>>> orderLinkMap = new HashMap<>();
+        try (Connection connection = connectionPool.getConnection()){
+            Map<Integer, Material> materialMap = getMaterialMap(connection);
+            String sql = "SELECT * FROM orderLinking";
+
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ResultSet rs = ps.executeQuery();
+
+                while (rs.next()) {
+                    int orderId = rs.getInt(2);
+                    int materialId = rs.getInt(3);
+                    int materialAmount = rs.getInt(4);
+
+                    Pair<Material, Integer> materialPair = new Pair<>(materialMap.get(materialId), materialAmount);
+                    List<Pair<Material, Integer>> materialPairs = new ArrayList<>();
+
+                    if (orderLinkMap.containsKey(orderId)) {
+                        materialPairs = orderLinkMap.get(orderId);
+                    }
+
+                    materialPairs.add(materialPair);
+
+                    orderLinkMap.put(orderId, materialPairs);
+                }
+            }
+
+
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new DatabaseException(e.getMessage());
         }
 
-        return null;
+        return orderLinkMap;
     }
 
     private static Map<Integer, Material> getMaterialMap(Connection connection) throws DatabaseException {
         Map<Integer, Material> materialMap = new HashMap<>();
-        String sql = "SELECT * FROM semesteropgave.material";
+        String sql = "SELECT * FROM material";
 
         try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ResultSet rs = ps.executeQuery();
@@ -108,8 +134,17 @@ class MaterialMapper {
             while (rs.next()) {
                 int id = rs.getInt(1);
                 String name = rs.getString(2);
+                int width = rs.getInt(3);
+                int height = rs.getInt(4);
+                int length = rs.getInt(5);
+                String description = rs.getString(6);
+                double price = rs.getDouble(7);
 
-                System.out.println("ID: " + id + ", Name: " + name);
+                Material m = new Material(name, width, height, length, description);
+                m.setId(id);
+                m.setPricePrMeter(price);
+
+                materialMap.put(id, m);
             }
         } catch (SQLException throwables) {
             throw new DatabaseException(throwables.getMessage());
