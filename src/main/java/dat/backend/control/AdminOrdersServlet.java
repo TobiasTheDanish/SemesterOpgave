eca60d97b6c2c6b8b82b30b6ba4a7d8a0d7f7525
TobiasTheDanish <t.hc199811@gmail.com>
2023-05-19 10:47:09 +0200

    Removed unused imports

--- src/main/java/dat/backend/control/AdminOrdersServlet.java
+++ src/main/java/dat/backend/control/AdminOrdersServlet.java
@@ -1,21 +1,16 @@
 package dat.backend.control;
 
 import dat.backend.model.config.ApplicationStart;
-import dat.backend.model.entities.Material;
 import dat.backend.model.entities.Order;
 import dat.backend.model.exceptions.DatabaseException;
 import dat.backend.model.persistence.ConnectionPool;
-import dat.backend.model.persistence.MaterialFacade;
 import dat.backend.model.persistence.OrderFacade;
-import org.javatuples.Pair;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @WebServlet(name = "adminordersservlet", value = "/adminordersservlet")
 public class AdminOrdersServlet extends HttpServlet {
@@ -30,10 +25,9 @@ public class AdminOrdersServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
-        List<Order> orders = (List<Order>) session.getAttribute("orders");
 
         try {
-            orders = OrderFacade.getAllOrdersWithoutMaterials(connectionPool);
+            List<Order> orders = OrderFacade.getAllOrders(connectionPool);
 
             session.setAttribute("orders", orders);
 
@@ -42,16 +36,6 @@ public class AdminOrdersServlet extends HttpServlet {
             e.printStackTrace();
             request.setAttribute("errormessage", "Der skete en fejl under hentningen af ordrer fra databasen:\n" + e.getMessage());
             request.getRequestDispatcher("error.jsp").forward(request, response);
-        }
-
-        try {
-            Map<Integer, List<Pair<Material, Integer>>> orderLinkMap = MaterialFacade.getOrderLinkMap(connectionPool);
-
-            for (Order order : orders) {
-                order.setMaterials(orderLinkMap.get(order.getId()));
-            }
-        } catch (DatabaseException e) {
-            e.printStackTrace();
         }
     }
 }
